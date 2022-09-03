43e61f50cecdf1fed142a3d66027677e4401e8df
Anthony <cooperkl317@gmail.com>
2022-09-02 18:47:55 -0700

    added index to book cards

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let myLibrary = [];
 
 let form = document.getElementById('bookForm');
-
 let content = document.getElementById('content');
 let addBookButton = document.getElementById('addBookButton');
 let modal = document.getElementById('modal');
@@ -16,43 +15,6 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    makeBookCard(book);
-}
-
-function makeBookCard(book) {
-    const bookCard = document.createElement('div');
-    const infoList = document.createElement('ul');
-    const bookTitle = document.createElement('li');
-    const bookAuthor = document.createElement('li');
-    const bookPages = document.createElement('li');
-    const bookRead = document.createElement('button');
-    const bookRemove = document.createElement('button');
-
-    infoList.classList.add('infoList');
-    bookCard.classList.add('bookCard');
-    bookTitle.classList.add('bookTitle');
-    bookAuthor.classList.add('bookAuthor');
-    bookPages.classList.add('bookPages');
-    bookRead.classList.add('bookRead');
-    bookRemove.classList.add('bookRemove');
-
-    bookTitle.textContent = `Title: ${book.title}`;
-    bookAuthor.textContent = `Author: ${book.author}`;
-    bookPages.textContent = `Pages: ${book.pages}`;
-    bookRead.textContent = `Read`;
-    bookRemove.textContent = `Remove`;
-
-    content.appendChild(bookCard);
-    bookCard.appendChild(infoList);
-    infoList.appendChild(bookTitle);
-    infoList.appendChild(bookAuthor);
-    infoList.appendChild(bookPages);
-    bookCard.appendChild(bookRead);
-    bookCard.appendChild(bookRemove);
-}
-
 function newBook() {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
@@ -65,10 +27,44 @@ function newBook() {
     }
 
     let book = new Book(title, author, pages, read)
-    addBookToLibrary(book);
+    myLibrary.push(book);
 }
 
+function addCard() {
+    content.innerHTML = '';
+    myLibrary.forEach(function(item) {
+        const bookCard = document.createElement('div');
+        const infoList = document.createElement('ul');
+        const bookTitle = document.createElement('li');
+        const bookAuthor = document.createElement('li');
+        const bookPages = document.createElement('li');
+        const bookRead = document.createElement('button');
+        const bookRemove = document.createElement('button');
+    
+        infoList.classList.add('infoList');
+        bookCard.classList.add('bookCard');
+        bookTitle.classList.add('bookTitle');
+        bookAuthor.classList.add('bookAuthor');
+        bookPages.classList.add('bookPages');
+        bookRead.classList.add('bookRead');
+        bookRemove.classList.add('bookRemove');
 
+        bookCard.dataset.volume = myLibrary.indexOf(item);
+        bookTitle.textContent = `Title:  ${item.title}`;
+        bookAuthor.textContent = `Author:  ${item.author}`;
+        bookPages.textContent = `Pages:  ${item.pages}`;
+        bookRead.textContent = `${item.read}`;
+        bookRemove.textContent = `Remove`;
+    
+        content.appendChild(bookCard);
+        bookCard.appendChild(infoList);
+        infoList.appendChild(bookTitle);
+        infoList.appendChild(bookAuthor);
+        infoList.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+        bookCard.appendChild(bookRemove);             
+        });
+}
 
 function openModal() {
     modal.classList.add('active');
@@ -78,30 +74,26 @@ function closeModal() {
     modal.classList.remove('active');
 }
 
-
-function submitBook() {
-    newBook();
-    form.reset();
-    closeModal();
-
+function toggleRead(book) {
+    if(book.read == 'Read') {
+        book.read = 'Not Read';
+    } else {
+        book.read = 'Read';
+    }
 }
 
 addBookButton.addEventListener('click', function() {
     openModal();
-
 });
 
-submitButton.addEventListener('click', function() {
-    submitBook();
+submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    newBook();
+    addCard();
+    form.reset();
+    closeModal();
 });
 
 exitButton.addEventListener('click', function() {
     closeModal();
 })
-
-
-
-
-
-
-
